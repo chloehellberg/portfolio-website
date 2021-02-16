c748dbba9d564698a963a40f4c1039712f2b4a49
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-02-15 17:33:06 -0800

    Create initial files and start build.

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+// import Navbar from 'react-bootstrap/Navbar';
+import Routes from "./Routes";
+import Home from "./Home";
+
+
+// function App() {
+//   return (
+//     <div className="App container ">
+//       {/* <Navbar collapseOnSelect expand="lg" className="mb-6"> */}
+//       <Navbar>
+//         <Navbar.Brand className = "font-weight-bold">
+//           <h1>CHLOE HELLBERG</h1>
+//         </Navbar.Brand>
+//         <Navbar.Toggle />
+//       </Navbar>
+//       <Routes />
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <div className="chloegarb">
+          <h1>CHLOE HELLBERG</h1>
+      </div>
+      <Routes />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="home-lander">
+        <h1>Welcome to my website</h1>
+        <p>Here is info about my website</p>
+        <div className="hexagonB"></div>
+        <div className="hexagonM"></div>
+        <div className="hexagonT"></div>
+      </div>
+    </div>
+  );
+}
