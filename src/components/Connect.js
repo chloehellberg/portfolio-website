a4331ad48dbceee35f1d88de6c14a2ec902da516
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-02-16 17:51:39 -0800

    Start adding contact methods.

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Connect.css";
+import linkedIn from './../img/linkedIn.svg';
 
 
 export default function Connect() {
@@ -74,7 +75,27 @@ export default function Connect() {
         {renderForm()}
       </div>
       <div className="Contact">
-        
+      <div className="hex">
+          <div className="hexTop"></div>
+          <div className="hexMiddle">
+            {/* <img className="linkedIn" src={linkedIn} alt="LinkedIn" /> */}
+            <i class="fab fa-linkedin"></i>
+            <p>LinkedIn</p>
+          </div>
+          <div className="hexBottom"></div>
+          </div>
+        <div className="hex">
+          <div className="hexTop"></div>
+          <div className="hexMiddle"><br /><p>Twitter</p></div>
+          <div className="hexBottom"></div>
+        </div>
+        <div className=".hex-row">
+          <div className="hex">
+            <div className="hexTop"></div>
+            <div className="hexMiddle"><p>GitHub</p></div>
+            <div className="hexBottom"></div>
+          </div>
+        </div>
       </div>
     </div>
   )
