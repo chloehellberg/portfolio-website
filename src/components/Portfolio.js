35c9e0765194c1c22c528f514bd1d9c2f226c071
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-02-19 17:40:21 -0800

    Create portfolio cards and format update.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Card, Button } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
 import "./Portfolio.css";
-import * as Icon from 'react-bootstrap-icons';
+// import * as Icon from 'react-bootstrap-icons';
+import book from './../img/book.jpg';
+import plants2 from './../img/plants2.jpg';
+import lightBulb from './../img/lightBulb.jpg';
+import yoga from './../img/yoga.jpg';
+import tapHouse from './../img/tapHouse.jpg';
+import kitty from './../img/kitty.jpg';
+import computer from './../img/computer.jpg';
+import doggos from './../img/doggos.jpg';
+import alpaca from './../img/alpaca.jpg';
+import shapes from './../img/shapes.jpg';
 
 export default function Portfolio() {
 
@@ -16,10 +26,86 @@ export default function Portfolio() {
         <h2>Projects</h2>
       </div>
       <div className="projects">
-        <ul>
-          <section>Project 1</section>
-          <section>Project 2</section>
-        </ul>
+        <div className="projectOne">
+          <Card style={{ width: '17rem'}}>
+            <Card.Body>
+              <Card.Title>SUGGEST IT TO ME</Card.Title>
+              <Card.Text>ReactJS</Card.Text>
+              <Card.Img variant="top"src={book} alt="books" style={{ justifyContent: "center"}}  />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectTwo">
+        <Card style={{ width: '17em' }}>
+            <Card.Body>
+              <Card.Title>RE:SOURCE HUB</Card.Title>
+              <Card.Text>ReactJS + AWS</Card.Text>
+              <Card.Img variant="top" src={lightBulb} alt="lightbulb" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectThree">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>COUCH POTATO</Card.Title>
+              <Card.Text>C#/.NET</Card.Text>
+              <Card.Img variant="top" src={yoga} alt="yoga" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectFour">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>PLANT JOURNAL</Card.Title>
+              <Card.Text>ReactJS</Card.Text>
+              <Card.Img variant="top" src={plants2} alt="plants" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectFive">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>ANIMAL SHELTER</Card.Title>
+              <Card.Text>C#/.NET</Card.Text>
+              <Card.Img variant="top" src={doggos} alt="dogs" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectSix">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>TAP HOUSE</Card.Title>
+              <Card.Text>ReactJS + Redux</Card.Text>
+              <Card.Img variant="top" src={tapHouse} alt="taphouse" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectSeven">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>LOREM IPSUM</Card.Title>
+              <Card.Text>ReactJS + Redux</Card.Text>
+              <Card.Img variant="top" src={alpaca} alt="alpaca" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="projectEight">
+        <Card style={{ width: '17rem' }}>
+            <Card.Body>
+              <Card.Title>LOREM IPSUM</Card.Title>
+              <Card.Text>ReactJS + Redux</Card.Text>
+              <Card.Img variant="top" src={shapes} alt="shapes" style={{ justifyContent: "center"}} />
+
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </div>
   );
